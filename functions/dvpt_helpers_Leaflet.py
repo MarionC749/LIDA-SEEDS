c67750e84f2fa0706fa1411dccae4c3f8836adb8
Marion Carneiro <qxnq723@leeds.ac.uk>
2026-08-19 16:15:04 +0100

    added loading indicator, download data button, recentred map

--- functions/dvpt_helpers_Leaflet.py
+++ functions/dvpt_helpers_Leaflet.py
@@ -65,6 +65,7 @@ def dvpt_build_base_map():
         zoom= 11,
         style= {"width": "100%", "height": "100%"},
         children= [
+            
             dl.TileLayer( #adds background map tiles from OpenStreetMap
                 url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
                 attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>'
