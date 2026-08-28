41038556ef9011197908a6155c3ce5b67c742787
Marion Carneiro <qxnq723@leeds.ac.uk>
2026-08-28 14:42:18 +0100

    corrections

--- layouts/dvpt_layout_Leaflet.py
+++ layouts/dvpt_layout_Leaflet.py
@@ -29,12 +29,6 @@ def dvpt_map_layout():
                 }
             }
         ),
-        
-        #DUMMY
-        html.Div(
-            id="dvpt_map_resize_trigger",
-            style={"display": "none"}
-            ),
     
         # ------ Main Layout ------
         html.Div(
